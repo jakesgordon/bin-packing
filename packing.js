d2d7d324f7953bae9d2d2bca62f11b2ef35a7be5
Jake Gordon <jake@codeincomplete.com>
2011-05-05 07:16:21 -0700

    started drawing something on the canvas - make sure canvas logical width/height set correctly to avoid image scaling

--- packing.js
+++ packing.js
@@ -6,6 +6,16 @@ Packing = {
     {width: 16, height: 16, num: 5}
   ],
 
+  colors: [
+    "red",
+    "yellow",
+    "black",
+    "blue",
+    "pink",
+    "orange",
+    "green"
+  ],
+
   fields: {
   },
 
@@ -13,9 +23,11 @@ Packing = {
     if (Packing.supported()) {
       Packing.fields.go = $('#go');
       Packing.fields.blocks = $('#blocks');
+      Packing.fields.canvas = $('#canvas')[0];
+      Packing.fields.draw   = Packing.fields.canvas.getContext("2d");
       Packing.fields.go.click(Packing.run);
 
-      Packing.printBlocks(Packing.defaultBlocks);
+      Packing.saveBlocks(Packing.defaultBlocks);
     }
   },
 
@@ -33,11 +45,32 @@ Packing = {
   },
 
   run: function() {
-    var blocks = Packing.parseBlocks();
-    Packing.printBlocks(blocks);
+    var i, n, len, block, blocks = Packing.loadBlocks();
+    Packing.saveBlocks(blocks);
+    Packing.clearCanvas();
+    for (n = 0, len = blocks.length; n < len; n++) {
+      block = blocks[n];
+      for (i = 0 ; i < block.num ; i++) {
+        Packing.drawBlock(block, i*50, n*50, Packing.color(i));  
+      }
+    }
   },
 
-  parseBlocks: function() {
+  clearCanvas: function() {
+    Packing.fields.draw.clearRect(0, 0, Packing.fields.canvas.width, Packing.fields.canvas.height);
+  },
+
+  drawBlock: function(block, x, y, color) {
+    Packing.fields.draw.fillStyle = color;
+    Packing.fields.draw.strokeStyle = "none";
+    Packing.fields.draw.fillRect(x, y, block.width, block.height);
+  },
+
+  color: function(n) {
+    return Packing.colors[n % Packing.colors.length];
+  },
+
+  loadBlocks: function() {
     var n, len, block, blocks = Packing.fields.blocks.val().split("\n"), result = [];
     for(n = 0, len = blocks.length ; n < len ; n++) {
       block = blocks[n].split("x");
@@ -48,7 +81,7 @@ Packing = {
     return result;
   },
 
-  printBlocks: function(blocks) {
+  saveBlocks: function(blocks) {
     var n, len, block, str = "";
     for(n = 0, len = blocks.length ; n < len ; n++) {
       block = blocks[n];
@@ -60,3 +93,4 @@ Packing = {
 }
 
 $(Packing.init);
+
