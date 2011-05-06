516baf89289dd2afc818bfd5096ed067e7e6e985
Jake Gordon <jake@codeincomplete.com>
2011-05-06 15:57:13 -0700

    much refactoring and tidying up of algorithm

--- packing.js
+++ packing.js
@@ -1,17 +1,33 @@
-Packer = function(w, h) { this.init(w, h); };
+/*************************************************
+
+  TODO
+  ====
+   * grow to fit
+   * toggle colors on/off
+   * re-run on value change (and remove run button) 
+   * display whitespace ratio
+   * animation render 1 by 1
+
+
+**************************************************/
+
+Packer = function(w, h) {
+  this.init(w, h);
+};
+
 Packer.prototype = {
 
   init: function(w, h) {
     this.root = { x: 0, y: 0, w: w, h: h }
   },
 
-  place: function(w, h) {
-    var node = this.findNode(this.root, w, h);
-    if (node) {
-      this.splitNode(node, w, h);
-      return { x: node.x, y: node.y };
+  fit: function(blocks) {
+    var n, node, block;
+    for (n = 0; n < blocks.length; n++) {
+      block = blocks[n];
+      if (node = this.findNode(this.root, block.w, block.h))
+        block.fit = this.splitNode(node, block.w, block.h);
     }
-    return false;
   },
 
   findNode: function(root, w, h) {
@@ -27,6 +43,7 @@ Packer.prototype = {
     node.used = true;
     node.left  = { x: node.x,     y: node.y + h, w: node.w,     h: node.h - h };
     node.right = { x: node.x + w, y: node.y,     w: node.w - w, h: h          };
+    return node;
   }
 
 }
@@ -38,18 +55,6 @@ Packing = {
   colors: [ "#10F090", "#1090F0", "#90F010", "#9010F0", "#F01090", "#F09010" ],
   color:  function(n) { return Packing.colors[n % Packing.colors.length]; },
 
-  blocks: [
-    {w: 100, h: 100, num:   3},
-    {w:  60, h:  60, num:  10},
-    {w:  50, h:  20, num:  20},
-    {w:  20, h:  50, num:  20},
-    {w: 250, h: 250, num:   1},
-    {w: 250, h: 100, num:   1},
-    {w: 100, h: 250, num:   1},
-    {w:  10, h:  10, num: 100},
-    {w:   5, h:   5, num: 500}
-  ],
-
   //---------------------------------------------------------------------------
 
   init: function() {
@@ -58,16 +63,60 @@ Packing = {
       blocks: $('#blocks'),
       canvas: $('#canvas')[0],
       sort:   $('#sort'),
-      unfit:  $('#unfit')
+      go:     $('#go'),
+      nofit:  $('#nofit')
     };
 
     if (!Packing.el.canvas.getContext) // no support for canvas
       return false;
 
     Packing.el.draw = Packing.el.canvas.getContext("2d");
-    Packing.saveBlocks(Packing.blocks);
-    $('#go').click(Packing.run);
+    Packing.el.blocks.val(Packing.blocks.save(Packing.blocks.default));
+    Packing.el.blocks.change(Packing.run);
+    Packing.el.sort.change(Packing.run);
+    Packing.el.go.click(Packing.run);
     Packing.run();
+  },
+
+  //---------------------------------------------------------------------------
+
+  run: function() {
+
+    var i, n, len, pos, all, block, nofit = [];
+    var packer = new Packer(Packing.el.canvas.width-1, Packing.el.canvas.height-1);
+    var blocks = Packing.blocks.load(Packing.el.blocks.val());
+    var all    = blocks.expanded;
+
+    // sort
+    var sort = Packing.el.sort.val();
+    if (sort != 'none')
+      all.sort(Packing.sort[sort]);
+
+    // fit
+    packer.fit(all);
+
+    // draw
+    Packing.canvas.clear();
+    for (n = 0 ; n < all.length ; n++) {
+      block = all[n];
+      if (block.fit)
+        Packing.canvas.rect(block.fit.x, block.fit.y, block.w, block.h, Packing.color(n));
+      else
+        nofit.push(block)
+    }
+    Packing.canvas.boundary(packer.root);
+    Packing.showNoFit(nofit);
+  },
+
+  showNoFit: function(nofit) {
+    if (nofit.length > 0) {
+      for(n = 0 ; n < nofit.length ; n++)
+        nofit[n] = "" + nofit[n].w + "x" + nofit[n].h;
+      Packing.el.nofit.html("Did not fit (" + nofit.length + ") :<br>" + nofit.join(", ")).show();
+    }
+    else {
+      Packing.el.nofit.hide();
+    }
   },
 
   //---------------------------------------------------------------------------
@@ -85,49 +134,6 @@ Packing = {
     maxside : function (a,b) { var primary = Packing.sort.max(a,b); return (primary != 0) ? primary : Packing.sort.min(a,b); }
   },
 
-  run: function() {
-
-    var i, n, len, pos, all, block, blocks = Packing.loadBlocks(), packer = new Packer(Packing.el.canvas.width, Packing.el.canvas.height);
-    Packing.saveBlocks(blocks);
-    Packing.canvas.clear();
- 
-    all = []
-    for (n = 0; n < blocks.length; n++) {
-      block = blocks[n];
-      for (i = 0; i < block.num; i++) {
-        all.push({w: block.w, h: block.h});
-      }
-    }
-
-    var sort = Packing.el.sort.val();
-    if (sort != 'none')
-      all.sort(Packing.sort[sort]);
-
-    var unfit = [];
-    for (n = 0; n < all.length; n++) {
-      block = all[n];
-      pos = packer.place(block.w, block.h); 
-      if (pos) {
-        Packing.canvas.rect(pos.x, pos.y, block.w, block.h, Packing.color(n));
-      }
-      else {
-        unfit.push(block);
-      }
-    }
-
-    Packing.canvas.boundary(packer.root);
-
-    if (unfit.length > 0) {
-      for(n = 0 ; n < unfit.length ; n++)
-        unfit[n] = "" + unfit[n].w + "x" + unfit[n].h;
-      Packing.el.unfit.html("Did not fit (" + unfit.length + ") :<br>" + unfit.join(", ")).show();
-    }
-    else {
-      Packing.el.unfit.hide();
-    }
-
-  },
-
   //---------------------------------------------------------------------------
 
   canvas: {
@@ -143,9 +149,7 @@ Packing = {
     
     boundary: function(node) {
       if (node) {
-        Packing.el.draw.lineWidth = 1;
-        Packing.el.draw.strokeStyle = '#EEEEE';
-        Packing.el.draw.strokeRect(node.x - 0.5, node.y - 0.5, node.w, node.h);
+        Packing.el.draw.strokeRect(node.x + 0.5, node.y + 0.5, node.w, node.h);
         Packing.canvas.boundary(node.left);
         Packing.canvas.boundary(node.right);
       }
@@ -154,24 +158,44 @@ Packing = {
 
   //---------------------------------------------------------------------------
 
-  loadBlocks: function() {
-    var n, len, block, blocks = Packing.el.blocks.val().split("\n"), result = [];
-    for(n = 0, len = blocks.length ; n < len ; n++) {
-      block = blocks[n].split("x");
-      if (block.length >= 2) {
-        result.push({w: parseInt(block[0]), h: parseInt(block[1]), num: (block.length == 2 ? 1 : parseInt(block[2])) });
-      } 
-    }
-    return result;
-  },
+  blocks: {
 
-  saveBlocks: function(blocks) {
-    var n, len, block, str = "";
-    for(n = 0, len = blocks.length ; n < len ; n++) {
-      block = blocks[n];
-      str = str + block.w + "x" + block.h + (block.num > 1 ? "x" + block.num : "") + "\n";
+    default: [
+      {w: 100, h: 100, num:   3},
+      {w:  60, h:  60, num:  10},
+      {w:  50, h:  20, num:  20},
+      {w:  20, h:  50, num:  20},
+      {w: 250, h: 250, num:   1},
+      {w: 250, h: 100, num:   1},
+      {w: 100, h: 250, num:   1},
+      {w:  10, h:  10, num: 100},
+      {w:   5, h:   5, num: 500}
+    ],
+
+    load: function(val) {
+      var i, j, block, blocks = val.split("\n"), result = [];
+      for(i = 0 ; i < blocks.length ; i++) {
+        block = blocks[i].split("x");
+        if (block.length >= 2)
+          result.push({w: parseInt(block[0]), h: parseInt(block[1]), num: (block.length == 2 ? 1 : parseInt(block[2])) });
+      }
+      result.expanded = [];
+      for(i = 0 ; i < result.length ; i++) {
+        for(j = 0 ; j < result[i].num ; j++)
+          result.expanded.push({w: result[i].w, h: result[i].h});
+      }
+      return result;
+    },
+
+    save: function(blocks) {
+      var i, block, str = "";
+      for(i = 0; i < blocks.length ; i++) {
+        block = blocks[i];
+        str = str + block.w + "x" + block.h + (block.num > 1 ? "x" + block.num : "") + "\n";
+      }
+      return str;
     }
-    Packing.el.blocks.val(str);
+
   }
 
   //---------------------------------------------------------------------------
